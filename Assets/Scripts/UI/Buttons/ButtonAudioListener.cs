2e4a1b64b0cfa0f6017e11a8b705f4b295de5fa3
Jesco <JescoVogt@web.de>
2022-12-16 11:29:47 +0100

    Cool Main Menu Scene

---
+++ Assets/Scripts/UI/Buttons/ButtonAudioListener.cs
@@ -0,0 +1,29 @@
+﻿using Beta.Audio;
+using UnityEngine;
+
+namespace UI.Buttons {
+    
+    public class UIEventSoundListener : MonoBehaviour {
+        
+        [SerializeField] private SFXAudioEvent _uiHoverSfx;
+    
+        [SerializeField] private SFXAudioEvent _uiClickSfx;
+        
+        [SerializeField] private SFXManager _sfxManager;
+        
+        private void Start() {
+            CustomButton.OnAnyButtonHovered += HandleAnyButtonHovered;
+            CustomButton.OnAnyButtonClicked += HandleAnyButtonClicked;
+        }
+    
+    
+        private void OnDestroy() {
+            CustomButton.OnAnyButtonHovered -= HandleAnyButtonHovered;
+            CustomButton.OnAnyButtonClicked -= HandleAnyButtonClicked;
+        }
+     
+        
+        private void HandleAnyButtonHovered(CustomButton button) => _sfxManager.PlaySFX(_uiHoverSfx);
+        private void HandleAnyButtonClicked(CustomButton button) => _sfxManager.PlaySFX(_uiClickSfx);
+    }
+}
